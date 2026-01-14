c0b056332ccfeeeb5fdfa1ff4c48c435a4bd12f0
opendatame <ndeyebinetadieng11@gmail.com>
2026-01-14 00:39:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -259,6 +259,8 @@ def index():
 
 # ----------------------------
 # MAIN
-# ----------------------------
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
